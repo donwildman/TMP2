c1e8877a38e6bcd173bb459d6866681aad11cdd0
mhanowski <m.hanowski@mwaysolutions.com>
2013-01-08 18:55:02 +0100

    add object

---
+++ framework/modules/core/m.js
@@ -0,0 +1,7 @@
+(function( window ) {
+
+    window.M = {};
+
+    window.M.Version = window.M.version = '2.0';
+
+})(window);
---
+++ framework/modules/core/object.js
@@ -0,0 +1,75 @@
+M.Object = /** @scope M.Object.prototype */ {
+
+    /**
+     * The type of this object.
+     *
+     * @type String
+     */
+    type: 'M.Object',
+
+    _lastThis: null,
+
+    /**
+     * Creates an object based on a passed prototype.
+     *
+     * @param {Object} proto The prototype of the new object.
+     */
+    create: function( proto ) {
+        var f = function() {
+        };
+        f.prototype = proto;
+        return new f();
+    },
+
+    /**
+     * Includes passed properties into a given object.
+     *
+     * @param {Object} properties The properties to be included into the given object.
+     */
+    include: function( properties ) {
+        for( var prop in properties ) {
+            this[prop] = properties[prop];
+        }
+    },
+
+    /**
+     * Creates a new class and extends it with all functions of the defined super class
+     * The function takes multiple input arguments. Each argument serves as additional
+     * super classes - see mixins.
+     *
+     * @param {Object} properties The properties to be included into the given object.
+     */
+    extend: function( properties ) {
+        /* create the new object */
+        var obj = M.Object.create(this);
+
+        /* assign the properties passed with the arguments array */
+        obj.include(properties);
+
+        /* save the prototype in an extra variable */
+        obj._super = this;
+
+        /* return the new object */
+        return obj;
+    },
+
+    bindToCaller: function( caller, method, arg ) {
+        return function() {
+            if( Array.isArray(arg) ) {
+                return method.apply(caller, arg);
+            }
+            return method.call(caller, arg);
+        }
+    },
+
+    callFromSuper: function( functionName ) {
+        var bind = this._super;
+        if( M.Object._lastThis === this ) {
+            bind = bind._super;
+        } else {
+            M.Object._lastThis = this;
+        }
+        return this.bindToCaller(this, bind[functionName])();
+    }
+
+};
