0dcd0b68ce6b502e2b4a7ed961d5350603bf9c29
mhanowski <m.hanowski@mwaysolutions.com>
2013-01-10 10:28:01 +0100

    add a temporary node webserver to run test on the device
    Conflicts:
    
    	tools/webserver.js

---
+++ tools/webserver.js
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+
+//INFO: navigate to /TMP2/framework and run from there this script
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+
+    console.log('request starting...');
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        console.log(filePath, exists);
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+}).listen(8125);
+
+console.log('Server running at http://127.0.0.1:8125/');
