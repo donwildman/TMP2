b6ea39bc68f9fced8f30cc7128c92a93fa126f12
Frank Stierle <f.stierle@mwaysolutions.com>
2013-01-10 10:53:32 +0100

    added handleCallback to object

--- framework/modules/core/object.js
+++ framework/modules/core/object.js
@@ -53,12 +53,35 @@ M.Object = /** @scope M.Object.prototype */ {
         return obj;
     },
 
+    /**
+    * Binds a method to its caller, so it is always executed within the right scope.
+    *
+    * @param {Object} caller The scope of the method that should be bound.
+    * @param {Object} method The method to be bound.
+    * @param {Object} arg One or more arguments. If more, then apply is used instead of call.
+    */
     bindToCaller: function( caller, method, arg ) {
         return function() {
             if( Array.isArray(arg) ) {
                 return method.apply(caller, arg);
             }
             return method.call(caller, arg);
+        }
+    },
+
+    /**
+    * Calls a method defined by a handler
+    *
+    * @param {Object} handler A function, or an object including target and action to use with bindToCaller.
+    * @param {Object} arg One or more arguments.
+    */
+    handleCallback: function(handler, arg) {
+        if (typeof(handler) === 'function') {
+            handler(arg);
+        } else if (handler && handler.target && handler.action) {
+            var action = typeof(handler.action) === 'function' ? handler.action : handler.target[handler.action];
+            var call   = this.bindToCaller(handler.target, action, arg);
+            call();
         }
     },
 
