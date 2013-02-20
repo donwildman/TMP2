330bfe1bb6d2d7b4296f5385db2659c71bf9efcd
dominiklaubach <d.laubach@panacoda.com>
2013-02-20 16:31:40 +0100

    added M.Exception

---
+++ framework/modules/core/exception.js
@@ -0,0 +1,42 @@
+M.Exception = M.Object.extend(/** @scope M.Object.prototype */ {
+
+    /**
+     * The type of this object.
+     *
+     * @type String
+     */
+    type: 'M.Exception',
+
+    /**
+     * The message of the exception (defined by the concrete instance).
+     *
+     * @type String
+     */
+    message: '',
+
+    /**
+     * The name of the exception (defined by the concrete instance).
+     *
+     * @type String
+     */
+    name: '',
+
+    /**
+     * Return the exception object to be thrown by the application.
+     */
+    getException: function() {
+        var that = this;
+        return {
+            message: this.message,
+            name: this.name,
+            toString: function() {
+                return that.type + '.' + that.name;
+            }
+        }
+    }
+});
+
+M.Exception.INVALID_INPUT_PARAMETER = M.Exception.extend(/** @scope M.Exception.prototype */{
+    message: 'At least one input parameter doesn\'t match the expected criteria.',
+    name: 'INVALID_INPUT_PARAMETER'
+});
