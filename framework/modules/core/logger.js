ce872888c8e39035c453f0d854e232c5622f8817
stefanbuck <s.buck@panacoda.com>
2013-03-28 09:11:58 +0100

    implement fallback for time, timeEnd, count

--- framework/modules/core/logger.js
+++ framework/modules/core/logger.js
@@ -9,12 +9,12 @@
 M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
 
     /**
-     * A constant value for logging level: info.
+     * A constant value for logging level: log.
      *
      * @type Number
      * @private
      */
-    LEVEL_INFO: 0,
+    LEVEL_LOG: 0,
 
     /**
      * A constant value for logging level: debug.
@@ -47,40 +47,35 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      */
     type: 'M.Logger',
 
+
     /**
-     * This method is used to log a message on logging level debug.
      *
-     * @param {String} msg The logging message.
+     * @type Array
      */
-    log: function( msg ) {
-        this._print(msg, this.LEVEL_DEBUG);
-    },
+    _timeCollection: [],
+
+    /**
+     *
+     * @type Array
+     */
+    _countCollection: [],
 
     /**
      * This method is used to log a message on logging level debug.
      *
      * @param {String} msg The logging message.
      */
-    debug: function( msg ) {
-        this._print(msg, this.LEVEL_DEBUG);
+    log: function( msg, options ) {
+        this._print(msg, this.LEVEL_LOG, options);
     },
 
     /**
-     * This method is used to log a message on logging level error.
+     * This method is used to log a message on logging level debug.
      *
      * @param {String} msg The logging message.
      */
-    error: function( msg ) {
-        this._print(msg, this.LEVEL_ERROR);
-    },
-
-    /**
-     * This method is used to log a message on logging level warning.
-     *
-     * @param {String} msg The logging message.
-     */
-    warn: function( msg ) {
-        this._print(msg, this.LEVEL_WARN);
+    debug: function( msg, options ) {
+        this._print(msg, this.LEVEL_LOG, options);
     },
 
     /**
@@ -88,8 +83,26 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      *
      * @param {String} msg The logging message.
      */
-    info: function( msg ) {
-        this._print(msg, this.LEVEL_INFO);
+    info: function( msg, options ) {
+        this._print(msg, this.LEVEL_LOG, options);
+    },
+
+    /**
+     * This method is used to log a message on logging level warning.
+     *
+     * @param {String} msg The logging message.
+     */
+    warn: function( msg, options ) {
+        this._print(msg, this.LEVEL_WARN, options);
+    },
+
+    /**
+     * This method is used to log a message on logging level error.
+     *
+     * @param {String} msg The logging message.
+     */
+    error: function( msg, options ) {
+        this._print(msg, this.LEVEL_ERROR, options);
     },
 
     /**
@@ -100,12 +113,21 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
     time: function( label ) {
 
         // Are we in production mode, then do not throw any logs
-        if( this._appRunsInDebugMode() ) {
+        if( this._appRunsInNotDebugMode() ) {
             return;
         }
 
+        // Fallback if the browser doesn't support time
         if( typeof console === 'undefined' || typeof console.time === 'undefined' ) {
-            // TODO: Implement fallback
+            var item = _.find(this._timeCollection, function( item ) {
+                return item.label === label;
+            });
+            if( !item ) {
+                this._timeCollection.push({
+                    label: label,
+                    time: new Date().getTime()
+                });
+            }
             return;
         }
         console.time(label);
@@ -119,37 +141,83 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
     timeEnd: function( label ) {
 
         // Are we in production mode, then do not throw any logs
-        if( this._appRunsInDebugMode() ) {
+        if( this._appRunsInNotDebugMode() ) {
             return;
         }
 
+        // Fallback if the browser doesn't support timeEnd
         if( typeof console === 'undefined' || typeof console.time === 'undefined' ) {
-            // TODO: Implement fallback
+            var item = _.find(this._timeCollection, function( item ) {
+                return item.label === label;
+            });
+            if( item ) {
+                var now = new Date().getTime();
+                var diff = (now - item.time) / 1000;
+                var index = this._timeCollection.indexOf(item);
+                this._print(item.label + ': ' + diff + 'ms', this.LEVEL_DEBUG);
+                this._timeCollection.splice(index, 1);
+            }
+
             return;
         }
         console.timeEnd(label);
     },
 
     /**
-     * This method is used to log anything out of an application based on the given logging level.
-     * Possible values for the logging level are:
      *
-     * - debug:   M.LEVEL_DEBUG
-     * - error:   M.ERROR
-     * - warning: M.LEVEL_WARN
-     * - info: M.LEVEL_INFO
+     * @param {String}
+     */
+    count: function( label ) {
+
+        // Are we in production mode, then do not throw any logs
+        if( this._appRunsInNotDebugMode() ) {
+            return;
+        }
+
+        // Fallback if the browser doesn't support count
+        if( typeof console === 'undefined' || typeof console.count === 'undefined' ) {
+            var item = _.find(this._countCollection, function( item ) {
+                return item.label === label;
+            });
+            if( item === undefined ) {
+                this._countCollection.push({
+                    label: label,
+                    count: 1
+                });
+                item = _.last(this._countCollection);
+            } else {
+                item.count++;
+            }
+
+            this._print(item.label + ': ' + item.count, this.LEVEL_DEBUG);
+
+            return;
+        }
+        console.count(label);
+    },
+
+    /**
+     * This method is used to log anything out of an application based on the given logging level.
      *
      * @param {String} msg The logging message.
      * @param {Number} level The logging level.
      * @private
      */
-    _print: function( msg, level ) {
-        level = level || this.LEVEL_DEBUG;
+    _print: function( msg, level, options ) {
 
         // Are we in production mode, then do not throw any logs
-        if( this._appRunsInDebugMode() ) {
+        if( this._appRunsInNotDebugMode() ) {
             return;
         }
+
+        // Assign default level if level is undefined
+        level = level || this.LEVEL_LOG;
+
+        var defaultOptions = {
+            // TODO: Add default options e.g. printDate
+        }
+
+        var settings = _.extend(defaultOptions, options);
 
         /* Prevent a console.log from blowing things up if we are on a browser that doesn't support this. */
         if( typeof console === 'undefined' ) {
@@ -159,20 +227,20 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
         }
 
         switch( level ) {
-            case this.LEVEL_DEBUG:
-                console.debug(msg);
-                break;
-            case this.LEVEL_ERROR:
-                console.error(msg);
+            case this.LEVEL_LOG:
+                console.log(msg);
                 break;
             case this.LEVEL_WARN:
                 console.warn(msg);
                 break;
-            case this.LEVEL_INFO:
-                console.info(msg);
+            case this.LEVEL_ERROR:
+                console.error(msg);
+                break;
+            case this.LEVEL_DEBUG:
+                console.debug(msg);
                 break;
             default:
-                console.debug(msg);
+                console.log(msg);
                 break;
         }
     },
@@ -183,9 +251,9 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      * @returns {boolean}
      * @private
      */
-    _appRunsInDebugMode: function() {
+    _appRunsInNotDebugMode: function() {
         // TODO: Get debugMode form config
-        return false;
+        return NO;
     }
 
 });
