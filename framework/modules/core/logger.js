d93424b05e0e07a9fe5c3d9da7e58b519ec4b322
stefanbuck <web@broud.de>
2013-04-23 23:28:04 +0200

    optimize logger renamed methods and properties

--- framework/modules/core/logger.js
+++ framework/modules/core/logger.js
@@ -12,34 +12,36 @@
 /**
  * @class
  *
- * M.Logger defines the prototype for any logging object. It is used to log messages out of the application
- * based on a given logging level.
+ * M.Logger defines the prototype for any logging object.
+ * It is used to log messages out of the application.
  *
  * @extends M.Object
  */
 M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
 
     /**
-     * @type Array / String
+     * Specifies which tags are displayed in the console.
+     * Leave this properties empty to display all logs.
+     *
+     * M.Logger.filter('tag1');
+     *
+     * M.Logger.log('Init Module', 'tag1');               // will displayed
+     * M.Logger.log('Get environment', 'tag2');           // will not displayed
+     * M.Logger.log('Loading data', ['tag1', 'tag2']);    // will displayed
+     * M.Logger.log('Loading images', ['tag2', 'tag3']);  // will not displayed
+     *
+     * @type Array
      */
     filter: [],
 
     /**
+     * A constant value for tag type: empty.
+     *
      * @type Sring
      */
-    TAG_ALL: 'all',
+    TAG_EMPTY: '',
 
     /**
-     * @type Sring
-     */
-    TAG_FRAMEWORK_CORE: 'framework-core',
-
-    /**
-     * @type Sring
-     */
-    TAG_FRAMEWORK_UI: 'framework-ui',
-
-     /**
      * The type of this object.
      *
      * @type String
@@ -47,36 +49,36 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
     _type: 'M.Logger',
 
     /**
-     * A constant value for logging level: log.
+     * A constant value for logging output: log.
      *
      * @type Number
      * @private
      */
-    _LEVEL_LOG: 0,
+    _OUTPUT_LOG: 0,
 
     /**
-     * A constant value for logging level: debug.
+     * A constant value for logging output: warning.
      *
      * @type Number
      * @private
      */
-    _LEVEL_DEBUG: 1,
+    _OUTPUT_DEBUG: 1,
 
     /**
-     * A constant value for logging level: warning.
+     * A constant value for logging output: warning.
      *
      * @type Number
      * @private
      */
-    _LEVEL_WARN: 2,
+    _OUTPUT_WARN: 2,
 
     /**
-     * A constant value for logging level: error.
+     * A constant value for logging output: error.
      *
      * @type Number
      * @private
      */
-    _LEVEL_ERROR: 3,
+    _OUTPUT_ERROR: 3,
 
     /**
      * A constant value for logging level: timeEnd.
@@ -87,63 +89,69 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
     _LEVEL_TIME_END: 4,
 
     /**
-     * A constant value for logging level: count.
-     *
-     * @type Number
-     * @private
-     */
-    _LEVEL_COUNT: 5,
-
-    /**
+     * This property holds the fallback entries for _time() / _timeEnd()
      *
      * @type Array
      */
     _times: [],
 
     /**
+     * This property holds the fallback entries for _count()
      *
      * @type Array
      */
     _counts: [],
 
     /**
+     * This property holds the debugMode from the config
+     *
+     * @type Boolean
+     */
+    _appRunsInNotDebugMode: NO,
+
+    /**
+     * Constructor method for M.Logger
+     */
+    init: function () {
+
+        // Prevent a console.log from blowing things up if we are on a browser that doesn't support this.
+        if (_.isUndefined(console)) {
+            window.console = {};
+            console.log = console.debug = console.warn = console.error = function () {};
+        }
+
+        // Check if app runs in debug mode
+        // TODO: Get debugMode form config
+        this._appRunsInNotDebugMode = NO;
+    },
+
+    /**
      * This method is used to log a message on logging level debug.
      *
-     * @param {String} msg The logging message.
+     * @param {String} message The logging message.
      * @param {String/Array} tag
      */
-    log: function( msg, tag ) {
-        this._print(msg, this._LEVEL_LOG, tag);
+    log: function (message, tag) {
+        this._print(this._OUTPUT_LOG, message, tag);
     },
 
     /**
      * This method is used to log a message on logging level warning.
      *
-     * @param {String} msg The logging message.
+     * @param {String} message The logging message.
      * @param {String/Array} tag
      */
-    warn: function( msg, tag ) {
-        this._print(msg, this._LEVEL_WARN, tag);
+    warn: function (message, tag) {
+        this._print(this._OUTPUT_WARN, message, tag);
     },
 
     /**
      * This method is used to log a message on logging level error.
      *
-     * @param {String} msg The logging message.
-     * @param {String/Array} tag
+     * @param {String} message The logging message.
      */
-    error: function( msg, tag ) {
-        this._print(msg, this._LEVEL_ERROR, tag);
-    },
-
-    /**
-     * This method is used to log a message on logging level debug.
-     *
-     * @param {String} msg The logging message.
-     * @param {String/Array} tag
-     */
-    debug: function( msg, tag ) {
-        this._print(msg, this._LEVEL_DEBUG, tag);
+    error: function (message) {
+        this._print(this._OUTPUT_ERROR, message);
     },
 
     /**
@@ -151,15 +159,15 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      *
      * @param {String}
      */
-    time: function( label ) {
+    time: function (label) {
 
         // Are we in production mode, then do not throw any logs
-        if( this._appRunsInNotDebugMode() ) {
+        if (this._appRunsInNotDebugMode) {
             return;
         }
 
         // Fallback if the browser doesn't support time
-        if( _.isUndefined(console.time) ) {
+        if (_.isUndefined(console.time)) {
             this._time(label);
             return;
         }
@@ -171,20 +179,19 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      *
      * @param {String}
      */
-    timeEnd: function( label ) {
+    timeEnd: function (label) {
 
         // Are we in production mode, then do not throw any logs
-        if( this._appRunsInNotDebugMode() ) {
+        if (this._appRunsInNotDebugMode) {
             return;
         }
 
         // Fallback if the browser doesn't support timeEnd
-        if( _.isUndefined(console.timeEnd) ) {
+        if (_.isUndefined(console.timeEnd)) {
             this._timeEnd(label);
             return;
         }
         console.timeEnd(label);
-        //this._print(label, this._LEVEL_TIME_END);
     },
 
     /**
@@ -192,78 +199,70 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      *
      * @param {String}
      */
-    count: function( label ) {
+    count: function (label) {
 
         // Are we in production mode, then do not throw any logs
-        if( this._appRunsInNotDebugMode() ) {
+        if (this._appRunsInNotDebugMode) {
             return;
         }
 
         // Fallback if the browser doesn't support count
-        if( _.isUndefined(console.count) ) {
+        if (_.isUndefined(console.count2)) {
             this._count(label);
             return;
         }
-        this._print(label, this._LEVEL_COUNT);
+        console.count(label);
     },
 
     /**
      * This method is used to log anything out of an application based on the given logging level.
      *
-     * @param {String} msg The logging message.
-     * @param {Number} level The logging level.
+     * @param {String} message The logging message.
+     * @param {Number} output The logging level.
      * @param {String/Array} tag
      * @private
      */
-    _print: function( msg, level, tag ) {
+    _print: function (output, message, tag) {
 
         // Are we in production mode, then do not throw any logs
-        if( this._appRunsInNotDebugMode() ) {
+        if (this._appRunsInNotDebugMode) {
             return;
         }
 
         // Assign default level if level is undefined
-        level = level || this._LEVEL_LOG;
+        output = output || this._OUTPUT_LOG;
 
         // Assign default tag if tag is undefined
-        tag = tag || this.TAG_ALL;
+        tag = tag || this.TAG_EMPTY;
 
-        if( this._preventPrintByTag(tag) ) {
+        if (this._preventOutputByTag(tag)) {
             return;
         }
 
-        /* Prevent a console.log from blowing things up if we are on a browser that doesn't support this. */
-        if( _.isUndefined(console) ) {
-            window.console = {};
-            console.log = console.info = console.warn = console.error = function() {
-            };
+        if (output < this._OUTPUT_ERROR) {
+            if (_.isArray(tag) && this.filter.indexOf(this.TAG_EMPTY) === -1) {
+                var tagString = _.without(this.filter, tag);
+                message = '[' + tagString + '] ' + message;
+            } else if (tag.length > 0) {
+                message = '[' + tag + '] ' + message;
+            }
         }
 
-        if( level < this._LEVEL_DEBUG) {
-            msg = '[' + tag + '] '+msg;
-        }
-
-        switch( level ) {
-            case this._LEVEL_LOG:
-                console.log(msg);
+        switch (output) {
+            case this._OUTPUT_LOG:
+                console.log(message);
                 break;
-            case this._LEVEL_WARN:
-                console.warn(msg);
+            case this._OUTPUT_WARN:
+                console.warn('WARNING: ' + message);
                 break;
-            case this._LEVEL_ERROR:
-                console.error(msg);
+            case this._OUTPUT_ERROR:
+                console.error('ERROR: ' + message);
                 break;
-            case this._LEVEL_DEBUG:
-                console.debug(msg);
-                break;
-            case this._LEVEL_TIME_END:
-                console.timeEnd(msg);
-                break;
-            case this._LEVEL_COUNT:
-                console.count(msg);
+            case this._OUTPUT_DEBUG:
+                console.debug(message);
                 break;
             default:
-                console.log(msg);
+                console.log(message);
                 break;
         }
     },
@@ -273,11 +272,11 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      *
      * @private
      */
-    _time: function( label ) {
-        var item = _.find(this._times, function( item ) {
+    _time: function (label) {
+        var item = _.find(this._times, function (item) {
             return item.label === label;
         });
-        if( !item ) {
+        if (!item) {
             this._times.push({
                 label: label,
                 time: new Date().getTime()
@@ -290,15 +289,15 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      *
      * @private
      */
-    _timeEnd: function( label ) {
-        var item = _.find(this._times, function( item ) {
+    _timeEnd: function (label) {
+        var item = _.find(this._times, function (item) {
             return item.label === label;
         });
-        if( item ) {
+        if (item) {
             var now = new Date().getTime();
             var diff = (now - item.time) / 1000;
             var index = this._times.indexOf(item);
-            this._print(item.label + ': ' + diff + 'ms', this._LEVEL_DEBUG);
+            this._print(this._OUTPUT_DEBUG, item.label + ': ' + diff + 'ms');
             this._times.splice(index, 1);
         }
     },
@@ -308,11 +307,11 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      *
      * @private
      */
-    _count: function( label ) {
-        var item = _.find(this._counts, function( item ) {
+    _count: function (label) {
+        var item = _.find(this._counts, function (item) {
             return item.label === label;
         });
-        if( item === undefined ) {
+        if (item === undefined) {
             this._counts.push({
                 label: label,
                 count: 1
@@ -322,18 +321,7 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
             item.count++;
         }
 
-        this._print(item.label + ': ' + item.count, this._LEVEL_DEBUG);
-    },
-
-    /**
-     * Check if app runs in debug mode
-     *
-     * @returns {boolean}
-     * @private
-     */
-    _appRunsInNotDebugMode: function() {
-        // TODO: Get debugMode form config
-        return NO;
+        this._print(this._OUTPUT_DEBUG, item.label + ': ' + item.count);
     },
 
     /**
@@ -343,14 +331,14 @@ M.Logger = M.Object.extend(/** @scope M.Logger.prototype */ {
      * @returns {boolean}
      * @private
      */
-    _preventPrintByTag: function( tag ) {
-        if( this.filter.length > 0 && this.filter.indexOf(this.TAG_ALL) === -1 ) {
-            if( _.isString(tag) ) {
-                if( this.filter.indexOf(tag) === -1 ) {
+    _preventOutputByTag: function (tag) {
+        if (this.filter.length > 0 && this.filter.indexOf(this.TAG_EMPTY) === -1) {
+            if (_.isString(tag)) {
+                if (this.filter.indexOf(tag) === -1) {
                     return YES;
                 }
-            } else if( _.isArray(tag) ) {
-                if( _.difference(tag, this.filter).length === tag.length ) {
+            } else if (_.isArray(tag)) {
+                if (_.difference(tag, this.filter).length === tag.length) {
                     return YES;
                 }
             }
